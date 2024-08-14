adbf8ef43e7e7bdef763b4a79e9312bc0984b7e1
Simeon Tran <siatran@ucsc.edu>
2024-08-14 15:32:51 -0700

    Print the path

--- delay/get_data.py
+++ delay/get_data.py
@@ -1,9 +1,9 @@
-import os
+from pathlib import Path
 
 CIPHERS = ["AES", "ASCON-128a", "ASCON-128", "No Encryption"]
 TX_POWERS = ["0 dBm", "9 dBm", "20 dBm"]
 
-RESULTS_PATH = os.path.join()
+RESULTS_PATH = Path(Path.home(), "Desktop", "Repositories", "Experiments", "delay", "data")
 
 """These files hold the Final Average Delays for each experiment.
 """
@@ -13,3 +13,6 @@ FILES={
 
 def getAverages():
   return
+
+if __name__ == "__main__":
+  print(RESULTS_PATH)
