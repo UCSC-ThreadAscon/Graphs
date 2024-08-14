bbec9bb8288fb2b50e4b6d11e49c9a624e3d5b50
Simeon Tran <siatran@ucsc.edu>
2024-08-14 15:59:31 -0700

    Was able to grab the final average delays.

--- delay/get_data.py
+++ delay/get_data.py
@@ -8,20 +8,20 @@ RESULTS_PATH = Path(Path.home(), "Desktop", "Repositories", "Experiments", "dela
 """These paths for the files which hold the Final Average Delays for each experiment.
 """
 DATA_FILEPATHS={
+  "No Encryption": {
+    "20 dBm": Path(RESULTS_PATH, "NoEncrypt-20dbm-trial-1", "delay-final-average-No Encrypt-20dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH, "NoEncrypt-9dbm-trial-1", "delay-final-average-No Encrypt-9dbm.txt"),
+    "0 dBm": None
+  },
   "AES": {
     "20 dBm": Path(RESULTS_PATH, "AES-20dbm-trial-1", "delay-final-average-AES-20dbm.txt"),
     "9 dBm": Path(RESULTS_PATH, "AES-9dbm-trial-1", "delay-final-average-AES-9dbm.txt"),
     "0 dBm": None
   },
-  "No Encryption": {
-    "20 dBm": Path(RESULTS_PATH, "NoEncrypt-20dbm-trial-1", "delay-final-average-NoEncrypt-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH, "NoEncrypt-9dbm-trial-1", "delay-final-average-NoEncrypt-9dbm.txt"),
-    "0 dBm": None
-  },
   "ASCON-128a": {
     "20 dBm": Path(RESULTS_PATH, "LibAscon-128a-20dbm-trial-1", "delay-final-average-ASCON-128a-20dbm.txt"),
-    "9 dBm": "",
-    "0 dBm": ""
+    "9 dBm": None,
+    "0 dBm": None
   },
   "ASCON-128": {
     "20 dBm": Path(RESULTS_PATH, "LibAscon-128-20dbm-trial-1", "delay-final-average-ASCON-128-20dbm.txt"),
@@ -36,4 +36,10 @@ def getAverages():
 if __name__ == "__main__":
   for cipher in DATA_FILEPATHS.keys():
     for txPower in DATA_FILEPATHS[cipher].keys():
-      print(DATA_FILEPATHS[cipher][txPower])
+      filepath = DATA_FILEPATHS[cipher][txPower]
+
+      if filepath != None:
+        with filepath.open("r") as file:
+          for line in file:
+            if "Final Average Delay under" in line:
+              print(line.strip("\n"))
