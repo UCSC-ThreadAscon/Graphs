0a36ef4a3465fa625be0191c3b6f5e6a7d83df9b
Simeon Tran <siatran@ucsc.edu>
2024-08-14 15:03:50 -0700

    Defining the constants.

---
+++ delay/get_data.py
@@ -0,0 +1,13 @@
+import os
+
+CIPHERS=["AES", "ASCON-128a", "ASCON-128", "No Encryption"]
+TX_POWERS=["0 dBm", "9 dBm", "20 dBm"]
+
+"""These files hold the Final Average Delays for each experiment.
+"""
+FILES={
+  ""
+}
+
+def getAverages():
+  return
