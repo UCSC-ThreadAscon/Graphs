0b460214b675d2915854cb4c397f87e54921b8d0
Simeon Tran <siatran@ucsc.edu>
2024-08-16 15:20:53 -0700

    Was able to grab the ratios.

--- delay/ratios.py
+++ delay/ratios.py
@@ -1,7 +1,13 @@
 from common import *
+from get_data import *
+"""TODO
+   Create a dictionary for AES, ASCON-128a, and ASCON-128, for each of the TX powers,
+   to store the % increases (or decreases) relative to No Encryption.
+"""
 
 """Given the final average delay between encryption vs. delay with no encryption,
-   this functionc calculates the % INCREASE in delay when using encryption vs. no encryption.
+   this functionc calculates the % INCREASE in delay when using encryption vs. no encryption,
+   RELATIVE to no encryption.
 
    NOTE that if the value returned is negative, the delay under encryption is LESS than
         no encryption. In this case, the value return should be considered as a % DECREASE
@@ -14,4 +20,14 @@ def getPercentIncrease(delayCipher, delayPlaintext):
   return percentDiff
 
 if __name__ == "__main__":
-  print(f"Percentage increase is {getPercentIncrease(20.3931, 19.4283):.5f} %.")
+  algorithms = ["AES", "ASCON-128a", "ASCON-128"]
+  averages = getAverages()
+
+  for algorithm in algorithms:
+    for txPower in TX_POWERS:
+      cipherAverage = averages[algorithm][txPower]
+      plaintextAverage = averages["No Encryption"][txPower]
+
+      if (cipherAverage != None) and (plaintextAverage != None):
+        percentIncrease = getPercentIncrease(cipherAverage, plaintextAverage)
+        print(f"{algorithm} % INCREASE relative to No Encryption @ {txPower}: {percentIncrease} %.")
