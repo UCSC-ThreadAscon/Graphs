226589ada59c368765521c42c585ed7e0d7669fc
Simeon Tran <siatran@ucsc.edu>
2024-08-16 14:13:28 -0700

    Docstring explanation, remove unncessary complexity.

--- delay/ratios.py
+++ delay/ratios.py
@@ -1,9 +1,13 @@
 from common import *
 
-def getPercentIncrease(delayCipher, delayPlaintext):
-  if (delayCipher < delayPlaintext):
-    raise Exception(f"Cannot Get Percent Increase: {delayCipher} us < {delayPlaintext} us.")
+"""Given the final average delay between encryption vs. delay with no encryption,
+   this functionc calculates the % INCREASE in delay when using encryption vs. no encryption.
 
+   NOTE that if the value returned is negative, the delay under encryption is LESS than
+        no encryption. In this case, the value return should be considered as a % DECREASE
+        in delay.
+"""
+def getPercentIncrease(delayCipher, delayPlaintext):
   usDiff = delayCipher - delayPlaintext
   ratioDiff = usDiff / delayPlaintext
   percentDiff = ratioDiff * 100
