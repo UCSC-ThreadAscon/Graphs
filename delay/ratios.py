3ffb7f46702fdfb96f7d83d9ff745432b3f01098
Simeon Tran <siatran@ucsc.edu>
2024-08-16 14:08:41 -0700

    Percentage increase function.

--- delay/ratios.py
+++ delay/ratios.py
@@ -4,7 +4,10 @@ def getPercentIncrease(delayCipher, delayPlaintext):
   if (delayCipher < delayPlaintext):
     raise Exception(f"Cannot Get Percent Increase: {delayCipher} us < {delayPlaintext} us.")
 
-  return
+  usDiff = delayCipher - delayPlaintext
+  ratioDiff = usDiff / delayPlaintext
+  percentDiff = ratioDiff * 100
+  return percentDiff
 
 if __name__ == "__main__":
-  print(getPercentIncrease(20.3931, 19.4283))
+  print(f"Percentage increase is {getPercentIncrease(20.3931, 19.4283):.5f} %.")
