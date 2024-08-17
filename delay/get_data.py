b1f62f92becc33b44340b2c3b12f248fd14b9ca3
Simeon Tran <siatran@ucsc.edu>
2024-08-16 21:27:38 -0700

    New result for ASCON-128a @ 9 dBm

--- delay/get_data.py
+++ delay/get_data.py
@@ -21,7 +21,7 @@ DATA_FILEPATHS={
   },
   "ASCON-128a": {
     "20 dBm": Path(RESULTS_PATH, "LibAscon-128a-20dbm-trial-1", "delay-final-average-ASCON-128a-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH, "LibAscon-128a-9dbm-trial-2", "delay-final-average-ASCON-128a-9dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH, "LibAscon-128a-9dbm-trial-3", "delay-final-average-ASCON-128a-9dbm.txt"),
     "0 dBm": Path(RESULTS_PATH, "LibAscon-128a-0dbm-trial-1", "delay-final-average-ASCON-128a-0dbm.txt")
   },
   "ASCON-128": {
