9876801a7cd5a4c2316bb12dbe8cb1e823936368
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-18 16:12:52 -0700

    Remove non-needed dictionary.

--- delay/data.py
+++ delay/data.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 SCRIPTS_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "delay")
 RESULTS_PATH_HOME = Path(Path.home(), "Desktop", "Repositories", "graphs", "delay", "data", "home-experiments")
+RESULTS_PATH_UCSC = Path(Path.home(), "Desktop", "Repositories", "graphs", "delay", "data", "ucsc-experiments")
 
 """These files hold the Delay averages for the experiments I ran at home.
 """
@@ -25,29 +26,6 @@ THESIS_DELAY_AVERAGES_HOME = {
     "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128-20dbm.txt"),
     "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128-9dbm.txt"),
     "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128-0dbm.txt")
-  }
-}
-
-THESIS_DELAY_AVERAGES_ALL = {
-  "No Encryption": {
-    "20 dBm": Path(RESULTS_PATH_HOME, "NoEncrypt-20dbm-trial-1", "delay-final-average-No Encrypt-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH_HOME, "NoEncrypt-9dbm-trial-1", "delay-final-average-No Encrypt-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH_HOME, "NoEncrypt-0dbm-trial-1", "delay-final-average-No Encrypt-0dbm.txt")
-  },
-  "AES": {
-    "20 dBm": Path(RESULTS_PATH_HOME, "AES-20dbm-trial-1", "delay-final-average-AES-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH_HOME, "AES-9dbm-trial-1", "delay-final-average-AES-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH_HOME, "AES-0dbm-trial-1", "delay-final-average-AES-0dbm.txt")
-  },
-  "ASCON-128a": {
-    "20 dBm": Path(RESULTS_PATH_HOME, "ASCON-128a-20dbm-trial-1", "delay-final-average-ASCON-128a-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH_HOME, "ASCON-128a-9dbm-trial-1", "delay-final-average-ASCON-128a-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH_HOME, "ASCON-128a-0dbm-trial-1", "delay-final-average-ASCON-128a-0dbm.txt")
-  },
-  "ASCON-128": {
-    "20 dBm": Path(RESULTS_PATH_HOME, "ASCON-128-20dbm-trial-1", "delay-final-average-ASCON-128-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH_HOME, "ASCON-128-9dbm-trial-1", "delay-final-average-ASCON-128-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH_HOME, "ASCON-128-0dbm-trial-1", "delay-final-average-ASCON-128-0dbm.txt")
   }
 }
 
