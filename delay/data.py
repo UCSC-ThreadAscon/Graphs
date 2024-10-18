58411330c406d716f2d502d163209d97a5b58682
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-18 16:17:26 -0700

    UCSC results

--- delay/data.py
+++ delay/data.py
@@ -52,9 +52,62 @@ THESIS_DELAY_LOGS_HOME = {
   }
 }
 
-LOCATION = "home"
+"""These files hold the Delay averages for the experiments I ran when I was at UCSC.
+"""
+THESIS_DELAY_AVERAGES_UCSC = {
+  "No Encryption": {
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-No Encrypt-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-No Encrypt-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-No Encrypt-0dbm.txt")
+  },
+  "AES": {
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-AES-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-AES-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-AES-0dbm.txt")
+  },
+  "ASCON-128a": {
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-ASCON-128a-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-ASCON-128a-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-ASCON-128a-0dbm.txt")
+  },
+  "ASCON-128": {
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-ASCON-128-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-ASCON-128-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "ucsc", "delay-final-average-FIRST-1000-ASCON-128-0dbm.txt")
+  }
+}
+
+THESIS_DELAY_LOGS_UCSC = {
+  "No Encryption": {
+    "20 dBm": Path(RESULTS_PATH_UCSC, "NoEncrypt-20dbm-trial-1", "delay-client-NoEncrypt-20dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH_UCSC, "NoEncrypt-9dbm-trial-1", "delay-client-NoEncrypt-9dbm.txt"),
+    "0 dBm": Path(RESULTS_PATH_UCSC, "NoEncrypt-0dbm-trial-1", "delay-client-NoEncrypt-0dbm.txt")
+  },
+  "AES": {
+    "20 dBm": Path(RESULTS_PATH_UCSC, "AES-20dbm-trial-1", "delay-client-AES-20dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH_UCSC, "AES-9dbm-trial-1", "delay-client-AES-9dbm.txt"),
+    "0 dBm": Path(RESULTS_PATH_UCSC, "AES-0dbm-trial-1", "delay-client-AES-0dbm.txt")
+  },
+  "ASCON-128a": {
+    "20 dBm": Path(RESULTS_PATH_UCSC, "ASCON-128a-20dbm-trial-1", "delay-client-LibAscon-128a-20dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH_UCSC, "ASCON-128a-9dbm-trial-1", "delay-client-LibAscon-128a-9dbm.txt"),
+    "0 dBm": Path(RESULTS_PATH_UCSC, "ASCON-128a-0dbm-trial-1", "delay-client-LibAscon-128a-0dbm.txt")
+  },
+  "ASCON-128": {
+    "20 dBm": Path(RESULTS_PATH_UCSC, "ASCON-128-20dbm-trial-1", "delay-client-LibAscon-128-20dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH_UCSC, "ASCON-128-9dbm-trial-1", "delay-client-LibAscon-128-9dbm.txt"),
+    "0 dBm": Path(RESULTS_PATH_UCSC, "ASCON-128-0dbm-trial-1", "delay-client-LibAscon-128-0dbm.txt")
+  }
+}
+
+
+LOCATION = "ucsc"
 
 if LOCATION == "home":
   THESIS_DELAY_AVERAGES = THESIS_DELAY_AVERAGES_HOME
   THESIS_DELAY_LOGS = THESIS_DELAY_LOGS_HOME
   LOCATION_STRING = "Home"
+elif LOCATION == "ucsc":
+  THESIS_DELAY_AVERAGES = THESIS_DELAY_AVERAGES_UCSC
+  THESIS_DELAY_LOGS = THESIS_DELAY_LOGS_UCSC
+  LOCATION_STRING = "UCSC"
