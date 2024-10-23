e0c131718d5e69b4d9d754a0539650582f71814e
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2024-10-23 14:43:48 -0700

    Throughput confirmable

---
+++ throughput-confirmable/average.py
@@ -0,0 +1,92 @@
+""" Calculates the average using ONLY the data from the FIRST 1000 TRIALS.
+"""
+import re
+import os
+import sys
+
+from data import *
+
+NUM_TRIALS = 1000
+
+def getAverageThroughputs(filepath):
+  averages = []
+  with open(filepath, 'r') as file:
+    for line in file:
+      if "The AVERAGE delay is:" in line:
+        words = line.split(" ")
+        average = float(words[7])
+        averages.append(average)
+
+  # Only return the first NUM_TRIALS trials.
+  return averages[0:NUM_TRIALS:]
+
+""" https://stackoverflow.com/a/35833387/6621292
+"""
+def getFinalAverage(averages):
+  listSum = 0
+  for average in averages:
+    listSum += average
+
+    if (listSum == sys.float_info.max):
+      raise OverflowError("Reach maxed float. Can't add anymore.")
+
+  return listSum / len(averages)
+
+def findFirstLine(expression, filepath):
+  with open(filepath, 'r') as file:
+    for line in file:
+      if expression in line:
+        return line
+  raise Exception(f"Can't find expression '{expression}' in '{filepath}'.")
+
+"""The lines of code and regular expression I used to remove ANSI escape
+   sequences comes from:
+   https://stackoverflow.com/a/14693789/6621292
+"""
+def removeAnsi(line):
+  ansiEscapes = re.compile(
+    br'(?:\x1B[@-Z\\-_]|[\x80-\x9A\x9C-\x9F]|(?:\x1B\[|\x9B)[0-?]*[ -/]*[@-~])'
+  )
+  result = ansiEscapes.sub(b'', bytes(line, "utf-8"))
+  return str(result, encoding="utf-8")
+
+def writeFinalAverage(averageDelays, finalAverage, delayExpLog):
+  line = findFirstLine("Cipher Suite:", delayExpLog)
+  words = line.split(" ")
+  cipher = removeAnsi(words[5]).replace('\n', '')
+
+  # If "No" gets parsed, it's actually supposed to be "No Encrypt".
+  # Since " " is used as a delimiter, the "Encrypt" word will be missed
+  # during the parsing process.
+  #
+  if cipher == "No":
+    cipher = "No Encrypt"
+
+  line = findFirstLine("Max TX Power is:", delayExpLog)
+  words = line.split(" ")
+  txPower = words[7]
+
+  outputFile = os.path.join(os.getcwd(), "final-averages", LOCATION, f"delay-final-average-FIRST-1000-{cipher}-{txPower}dbm.txt")
+
+  with open(outputFile, "w") as file:
+    file.write(f"Final Average Delay under {cipher} at {txPower} dBm: {finalAverage} us.\n")
+    file.write("List of Average Delays used to create the Final Average:\n")
+
+    trialNum = 1
+    for average in averageDelays:
+      file.write(f"Trial {trialNum}: {average} us")
+      trialNum += 1
+      if trialNum <= len(averageDelays):
+        file.write("\n")
+  return
+
+def getAllAverages():
+  for filesDict in THESIS_DELAY_LOGS.values():
+    for logFile in filesDict.values():
+      averages = getAverageThroughputs(logFile)
+      finalAverage = getFinalAverage(averages)
+      writeFinalAverage(averages, finalAverage, logFile)
+  return
+
+if __name__ == "__main__":
+  getAllAverages()
--- throughput-confirmable/data.py
+++ throughput-confirmable/data.py
@@ -7,24 +7,24 @@ RESULTS_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "throughpu
 """
 THESIS_DELAY_AVERAGES = {
   "No Encryption": {
-    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-No Encrypt-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-No Encrypt-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-No Encrypt-0dbm.txt")
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-No Encrypt-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-No Encrypt-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-No Encrypt-0dbm.txt")
   },
   "AES": {
-    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-AES-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-AES-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-AES-0dbm.txt")
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-AES-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-AES-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-AES-0dbm.txt")
   },
   "ASCON-128a": {
-    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128a-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128a-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128a-0dbm.txt")
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-ASCON-128a-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-ASCON-128a-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-ASCON-128a-0dbm.txt")
   },
   "ASCON-128": {
-    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "delay-final-average-FIRST-1000-ASCON-128-0dbm.txt")
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-ASCON-128-20dbm.txt"),
+    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-ASCON-128-9dbm.txt"),
+    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "home", "tp-con-final-average-ASCON-128-0dbm.txt")
   }
 }
 
