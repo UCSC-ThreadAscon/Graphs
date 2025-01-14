ee2778dd0821672f33ef186971948d13ad11a8e8
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-01-13 19:35:56 -0800

    New experiment graphs

--- delay/data.py
+++ delay/data.py
@@ -9,24 +9,24 @@ SCRIPTS_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "delay")
 
 THESIS_DELAY_AVERAGES = {
   "No Encryption": {
-    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-No Encrypt-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-No Encrypt-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-No Encrypt-0dbm.txt")
+    "20 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Jan-2024", "delay-final-average-FIRST-1000-No Encrypt-20dbm.txt"),
+    "9 dBm": None,
+    "0 dBm": None
   },
   "AES": {
     "20 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-AES-20dbm.txt"),
     "9 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-AES-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-AES-0dbm.txt")
+    "0 dBm": None
   },
   "ASCON-128a": {
     "20 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-ASCON-128a-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-ASCON-128a-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-ASCON-128a-0dbm.txt")
+    "9 dBm": None,
+    "0 dBm": None
   },
   "ASCON-128": {
     "20 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-ASCON-128-20dbm.txt"),
-    "9 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-ASCON-128-9dbm.txt"),
-    "0 dBm": Path(SCRIPTS_PATH, "final-averages", "Santa-Cruz-Dec-2024", "delay-final-average-FIRST-1000-ASCON-128-0dbm.txt")
+    "9 dBm": None,
+    "0 dBm": None
   }
 }
 
@@ -34,23 +34,23 @@ RESULTS_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "delay", "
 
 THESIS_DELAY_LOGS = {
   "No Encryption": {
-    "20 dBm": Path(RESULTS_PATH, "NoEncrypt-20dbm-trial-1", "delay-FTD-NoEncrypt-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH, "NoEncrypt-9dbm-trial-1", "delay-FTD-NoEncrypt-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH, "NoEncrypt-0dbm-trial-1", "delay-FTD-NoEncrypt-0dbm.txt")
+    "20 dBm": Path(RESULTS_PATH, "NoEncrypt-20dbm-trial-2", "delay-FTD-NoEncrypt-20dbm.txt"),
+    "9 dBm": None,
+    "0 dBm": None
   },
   "AES": {
-    "20 dBm": Path(RESULTS_PATH, "AES-20dbm-trial-2", "delay-FTD-AES-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH, "AES-9dbm-trial-1", "delay-FTD-AES-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH, "AES-0dbm-trial-2", "delay-FTD-AES-0dbm.txt")
+    "20 dBm": Path(RESULTS_PATH, "AES-20dbm-trial-3", "delay-FTD-AES-20dbm.txt"),
+    "9 dBm": Path(RESULTS_PATH, "AES-9dbm-trial-2", "delay-FTD-AES-9dbm.txt"),
+    "0 dBm": None
   },
   "ASCON-128a": {
-    "20 dBm": Path(RESULTS_PATH, "LibAscon-128a-20dbm-trial-1", "delay-FTD-LibAscon-128a-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH, "LibAscon-128a-9dbm-trial-1", "delay-FTD-LibAscon-128a-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH, "LibAscon-128a-0dbm-trial-1", "delay-FTD-LibAscon-128a-0dbm.txt")
+    "20 dBm": Path(RESULTS_PATH, "LibAscon-128a-20dbm-trial-2", "delay-FTD-LibAscon-128a-20dbm.txt"),
+    "9 dBm": None,
+    "0 dBm": None
   },
   "ASCON-128": {
-    "20 dBm": Path(RESULTS_PATH, "LibAscon-128-20dbm-trial-1", "delay-FTD-LibAscon-128-20dbm.txt"),
-    "9 dBm": Path(RESULTS_PATH, "LibAscon-128-9dbm-trial-1", "delay-FTD-LibAscon-128-9dbm.txt"),
-    "0 dBm": Path(RESULTS_PATH, "LibAscon-128-0dbm-trial-1", "delay-FTD-LibAscon-128-0dbm.txt")
+    "20 dBm": Path(RESULTS_PATH, "LibAscon-128-20dbm-trial-2", "delay-FTD-LibAscon-128-20dbm.txt"),
+    "9 dBm": None,
+    "0 dBm": None
   }
 }
