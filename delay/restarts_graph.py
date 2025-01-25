5515a81e7f3ff0a0b165e67cdce81fc874723248
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-01-25 13:24:07 -0800

    Bar graph for restarts graph

--- delay/restarts_graph.py
+++ delay/restarts_graph.py
@@ -4,6 +4,52 @@ import numpy as np
 from common import *
 from restarts import *
 
+SHOW_BAR_LABELS = True
+
+def bargraph():
+  xAxisValues = np.arange(len(TX_POWERS))
+  width = 0.2
+  multiplier = 0
+
+  figure, axis = plt.subplots(layout='constrained')
+
+  if RENDER_PGF:
+    figure.set_figwidth(THESIS_PAPER_WIDTH_IN / 1.2)
+    figure.set_figheight(THESIS_PAPER_HEIGHT_IN / 3)
+
+  for cipher, restartsDict in NUM_RESTARTS.items():
+    numRestarts = restartsDict.values()
+    offset = width * multiplier
+    rects = axis.bar(xAxisValues + offset, numRestarts, width, label=cipher,
+                  color=cipherColors[cipher])
+
+    if SHOW_BAR_LABELS:
+      axis.bar_label(rects, padding=3)
+
+    multiplier += 1
+
+  axis.set_ylabel('Number of Restarts')
+  axis.set_xlabel('TX Power (dBm)')
+  axis.set_title(f'Number of Experimental Trial Restarts (Delay)')
+
+  xWidthOffset = 0.30
+  axis.set_xticks(xAxisValues + xWidthOffset, TX_POWERS_LABELS.values())
+
+  y_min = 0
+  y_lim = 10
+
+  num_ticks = abs(y_lim - y_min) / 12
+  ticks = np.arange(0, y_lim, num_ticks)
+  ticks = np.append(ticks, [y_lim])
+
+  axis.set_yticks(ticks)
+  axis.legend(loc='best', ncols=4)
+  axis.set_ylim(y_min, y_lim)
+
+  if RENDER_PGF:
+    plt.savefig(os.path.join(THESIS_FIGURES_PATH, 'delay-bar-graph.pgf'))
+  return
+
 def linegraph():
   aes = NUM_RESTARTS['AES'].values()
   ascon128 = NUM_RESTARTS["ASCON-128"].values()
@@ -46,5 +92,5 @@ if __name__ == "__main__":
   getAllNumRestarts()
   print(NUM_RESTARTS)
 
-  linegraph()
+  bargraph()
   plt.show()
