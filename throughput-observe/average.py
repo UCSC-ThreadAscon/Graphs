13039b633fb977e9af675df978d0de80a78bbb0f
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-02-25 15:59:08 -0800

    Function

--- throughput-observe/average.py
+++ throughput-observe/average.py
@@ -29,6 +29,32 @@ def getFinalAverage(averages):
 
   return listSum / len(averages)
 
+def writeFinalAverage(averages, finalAverage, delayExpLog):
+  line = findFirstLine("Cipher Suite:", delayExpLog)
+  words = line.split(" ")
+  cipher = removeAnsi(words[5]).replace('\n', '')
+
+  if cipher == "No":
+    cipher = "No Encrypt"
+
+  line = findFirstLine("Max TX Power is:", delayExpLog)
+  words = line.split(" ")
+  txPower = words[7]
+
+  outputFile = os.path.join(os.getcwd(), "final-averages", f"tp-con-final-average-{cipher}-{txPower}dbm.txt")
+
+  with open(outputFile, "w") as file:
+    file.write(f"Final Average Throughput (Observe) under {cipher} at {txPower} dBm: {finalAverage} bytes/second.\n")
+    file.write("List of Average Throughputs (Observe) used to create the Final Average:\n")
+
+    trialNum = 1
+    for average in averages:
+      file.write(f"Trial {trialNum}: {average} bytes/second")
+      trialNum += 1
+      if trialNum <= len(averages):
+        file.write("\n")
+  return
+
 if __name__ == "__main__":
   averages = getAverages(THESIS_TP_OBSERVE_LOGS["ASCON-128"]["20 dBm"])
   finalAverage = getFinalAverage(averages)
