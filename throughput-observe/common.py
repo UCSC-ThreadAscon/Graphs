bfae94b0f7e352a30c6d2ea8de2c5e128f8865d7
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-02-25 15:42:10 -0800

    Common Python file

---
+++ throughput-observe/common.py
@@ -0,0 +1,35 @@
+import os
+import matplotlib
+
+RENDER_PGF = True
+
+if RENDER_PGF:
+  matplotlib.use("pgf")
+  matplotlib.rcParams.update({
+      "pgf.texsystem": "pdflatex",
+      'font.family': 'serif',
+      'text.usetex': True,
+      'pgf.rcfonts': False,
+  })
+
+THESIS_FIGURES_PATH = '/Users/simeon/Desktop/Repositories/ThesisWriteup/images/pgfs'
+THESIS_PAPER_WIDTH_IN = 5.75113
+THESIS_PAPER_HEIGHT_IN = 8.12659
+
+CIPHERS = ["AES", "ASCON-128a", "ASCON-128", "No Encryption"]
+TX_POWERS = ["0 dBm", "9 dBm", "20 dBm"]
+
+cipherColors = \
+{
+  'AES': 'deepskyblue',
+  'ASCON-128a': 'plum',
+  'ASCON-128': 'orange',
+  'No Encryption': 'mediumaquamarine'
+}
+
+TX_POWERS_LABELS = \
+{
+  "0dbm": "0 dBm",
+  "9dbm": "9 dBm",
+  "20dbm": "20 dBm"
+}
