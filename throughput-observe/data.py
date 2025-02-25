6b581c6c432e1a1c28ad474d7de0868f52533832
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-02-25 15:41:39 -0800

    Only save partial and successful trials

--- throughput-observe/data.py
+++ throughput-observe/data.py
@@ -8,22 +8,32 @@ RESULTS_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "throughpu
 
 THESIS_TP_OBSERVE_LOGS = {
   "No Encryption": {
-    "20 dBm": [],
+    "20 dBm": [
+      Path(SCRIPTS_PATH, "data", "NoEncrypt-20dbm-trial-1", "tp-observe-FTD-NoEncrypt-20dbm.txt")
+    ],
     "9 dBm": [],
     "0 dBm": []
   },
   "AES": {
-    "20 dBm": [],
+    "20 dBm": [
+      Path(SCRIPTS_PATH, "data", "AES-20dbm-trial-3", "tp-observe-FTD-AES-20dbm.txt")
+    ],
     "9 dBm": [],
     "0 dBm": []
   },
   "ASCON-128a": {
-    "20 dBm": [],
+    "20 dBm": [
+      Path(SCRIPTS_PATH, "data", "LibAscon-128a-20dbm-trial-1", "tp-observe-FTD-LibAscon-128a-20dbm.txt")
+    ],
     "9 dBm": [],
     "0 dBm": []
   },
   "ASCON-128": {
-    "20 dBm": [],
+    "20 dBm": [
+      Path(SCRIPTS_PATH, "data", "LibAscon-128-20dbm-trial-1", "tp-observe-FTD-LibAscon-128-20dbm.txt"),
+      Path(SCRIPTS_PATH, "data", "LibAscon-128-20dbm-Partial-1", "tp-observe-FTD-LibAscon-128-20dbm.txt"),
+      Path(SCRIPTS_PATH, "data", "LibAscon-128-20dbm-Partial-2", "tp-observe-FTD-LibAscon-128-20dbm.txt")
+    ],
     "9 dBm": [],
     "0 dBm": []
   },
