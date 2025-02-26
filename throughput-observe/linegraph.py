3cf94869e4bbf99d144472a15ec33d4048ca036d
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-02-25 16:31:44 -0800

    Progress

--- throughput-observe/linegraph.py
+++ throughput-observe/linegraph.py
@@ -11,9 +11,9 @@ def linegraph():
   ascon128 = RATIOS["ASCON-128"].values()
   ascon128a = RATIOS["ASCON-128a"].values()
 
-  y_interval = 1
-  y_lim = 1
-  y_min = -5
+  y_interval = 0.1
+  y_lim = 0.5
+  y_min = -0.5
 
   fig, ax = plt.subplots()
 
