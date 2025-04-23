650b0325d2103fdc085eea09b9375cacceecff76
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-04-23 11:56:55 -0700

    Print out current for first 100 samples.

--- energy/average.py
+++ energy/average.py
@@ -1,5 +1,6 @@
 import sys
 import csv
+
 from common import *
 from data import *
 
@@ -9,5 +10,21 @@ UA_WAKEUP_MINIMUM = MA_WAKEUP_MINIMUM * 1000
 """ TODO: Print the first 1000 entries of the PPK2 file.
 """
 
-def getAvgUa():
+def getAvgUa(filename):
+  with open(filename) as file:
+    reader = csv.DictReader(file)
+
+    i = 0
+    for row in reader:
+      if i >= 100:
+        break
+      else:
+        timestamp = float(row["Timestamp(ms)"])
+        uA = float(row["Current(uA)"])
+        print(f"At {timestamp} ms current was {uA} uA.")
+      i += 1
+
   return
+
+if __name__ == "__main__":
+  getAvgUa(THESIS_ENERGY_CSV["AES"]["20 dBm"])
--- energy/data.py
+++ energy/data.py
@@ -1,26 +1,26 @@
 from pathlib import Path
 
-CSV_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "throughput-observe", "csv")
+CSV_PATH = Path(Path.home(), "Desktop", "Repositories", "graphs", "energy", "csv")
 
 THESIS_ENERGY_CSV = {
   "No Encryption": {
-    "20 dBm": Path(CSV_PATHS, "NoEncrypt-20dbm-trial-1.csv"),
-    "9 dBm": Path(CSV_PATHS, "NoEncrypt-9dbm-trial-1.csv"),
-    "0 dBm": Path(CSV_PATHS, "NoEncrypt-0dbm-trial-1.csv")
+    "20 dBm": Path(CSV_PATH, "NoEncrypt-20dbm-trial-1.csv"),
+    "9 dBm": Path(CSV_PATH, "NoEncrypt-9dbm-trial-1.csv"),
+    "0 dBm": Path(CSV_PATH, "NoEncrypt-0dbm-trial-1.csv")
   },
   "AES": {
-    "20 dBm": Path(CSV_PATHS, "AES-20dbm-trial-1.csv"),
-    "9 dBm": Path(CSV_PATHS, "AES-9dbm-trial-1.csv"),
-    "0 dBm": Path(CSV_PATHS, "AES-0dbm-trial-1.csv")
+    "20 dBm": Path(CSV_PATH, "AES-20dbm-trial-1.csv"),
+    "9 dBm": Path(CSV_PATH, "AES-9dbm-trial-1.csv"),
+    "0 dBm": Path(CSV_PATH, "AES-0dbm-trial-1.csv")
   },
   "ASCON-128a": {
-    "20 dBm": Path(CSV_PATHS, "LibAscon-128a-20dbm-trial-1.csv"),
-    "9 dBm": Path(CSV_PATHS, "LibAscon-128a-9dbm-trial-1.csv"),
-    "0 dBm": Path(CSV_PATHS, "LibAscon-128a-0dbm-trial-1.csv")
+    "20 dBm": Path(CSV_PATH, "LibAscon-128a-20dbm-trial-1.csv"),
+    "9 dBm": Path(CSV_PATH, "LibAscon-128a-9dbm-trial-1.csv"),
+    "0 dBm": Path(CSV_PATH, "LibAscon-128a-0dbm-trial-1.csv")
   },
   "ASCON-128": {
-    "20 dBm": Path(CSV_PATHS, "LibAscon-128-20dbm-trial-2.csv"),
-    "9 dBm": Path(CSV_PATHS, "LibAscon-128-9dbm-trial-1.csv"),
-    "0 dBm": Path(CSV_PATHS, "LibAscon-128-0dbm-trial-1.csv")
+    "20 dBm": Path(CSV_PATH, "LibAscon-128-20dbm-trial-2.csv"),
+    "9 dBm": Path(CSV_PATH, "LibAscon-128-9dbm-trial-1.csv"),
+    "0 dBm": Path(CSV_PATH, "LibAscon-128-0dbm-trial-1.csv")
   }
 }
