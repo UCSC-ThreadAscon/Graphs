828869d88f25ea6404d814fc7e3f4571abb9963b
Simeon Tran <4740112+SimeonAT@users.noreply.github.com>
2025-04-24 17:13:02 -0700

    Simplify code for waveforms

--- waveforms/average.py
+++ waveforms/average.py
@@ -8,45 +8,22 @@ import sys
 import csv
 import math
 
-minToSec = lambda min : min * 60
-secToMs = lambda sec : sec * 1000
-minToMs = lambda min : secToMs(minToSec(min))
-minToHrs = lambda min : min / 60
-
-EXPERIMENT_DURATION_MINUTES = 183
-EXPERIMENT_DURATION_MS = minToMs(EXPERIMENT_DURATION_MINUTES)
-EXPERIMENT_DURATION_HRS = minToHrs(EXPERIMENT_DURATION_MINUTES)
-
 uAtoMa = lambda uA : uA * 0.001
-mAtoMah = lambda mA : mA * EXPERIMENT_DURATION_HRS
 mAtoUa = lambda mA : mA * 1000
 
 UA_WAKEUP_MINIMUM = 9
 
 def getSamples(filepath):
   uAWakeupList = []
-  tsPowerOn = None
 
   with open(filepath) as file:
     for row in csv.DictReader(file):
-      timestamp = float(row["Timestamp(ms)"])
       uA = float(row["Current(uA)"])
+      if uA >= UA_WAKEUP_MINIMUM:
+        uAWakeupList.append(uA)
 
-      if (tsPowerOn is None) and (uA >= 1): # Power on spike of ESP32-H2.
-        tsPowerOn = timestamp
-        print(f"Device power on detected @ {timestamp} ms with current {uA} uA.")
-
-      if tsPowerOn is not None:
-        if uA >= UA_WAKEUP_MINIMUM:
-          uAWakeupList.append(uA)
-
-        if len(uAList) >= sys.maxsize:
-          raise OverflowError("The list is too big.")
-
-        elapsed = timestamp - tsPowerOn
-        if (elapsed >= EXPERIMENT_DURATION_MS):
-          print(f"Stop post-processing @ {timestamp} ms with current {uA} uA.")
-          break
+      if len(uAList) >= sys.maxsize:
+        raise OverflowError("The list is too big.")
 
   return uAWakeupList
 
@@ -63,17 +40,14 @@ def getAvgUa(samples):
   average = accumulator / length
   return average
 
-def printAvgs(cipher, txPower):
-  filepath = None
+def printAvgs(filepath, cipher, txPower):
   uAList = getSamples(filepath)
   avgUa = getAvgUa(uAList)
   avgMa = uAtoMa(avgUa)
-  avgMah = mAtoMah(avgMa)
 
   print(f"---------- {cipher} {txPower} ----------")
   print(f"The average uA, deep sleep included, is {avgUa} uA.")
   print(f"The average mA, deep sleep included, is {avgMa} mA.")
-  print(f"The average mAh, deep sleep included, is {avgMah} mAh.")
   print("-------------------------------------------")
   return
 
